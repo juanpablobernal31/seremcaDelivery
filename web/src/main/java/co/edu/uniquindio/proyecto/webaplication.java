a5cac2efc45d4ff75fa79305e48599cae5358ba0
juanpablobernal31 <jpbernals@uqvirtual.edu.co>
2023-05-02 04:04:40 -0500

    se creo el modulo web para iniciar el desarrollo web de la aplicacion

---
+++ web/src/main/java/co/edu/uniquindio/proyecto/webaplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class webaplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(webaplication.class, args);
+    }
+
+}
